e62137e7cb805498339ddc673a497f35315f7212
Dutch Lamberson <dutch.lamberson@nasa.gov>
2023-08-11 11:22:25 -0400

    overhauls CI

--- ingrate.cc
+++ ingrate.cc
@@ -1,9 +1,46 @@
-#include <glad/gl.h>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <sstream>
 #include <GLFW/glfw3.h>
 
-int main() {
+namespace glfw {
+  namespace {
+    struct window_deleter {
+      void operator()(GLFWwindow *p) {
+        glfwDestroyWindow(p);
+      }
+    };
+  }
+
+  class window {
+    std::unique_ptr<GLFWwindow, window_deleter> p;
+
+  public:
+    template<typename... Args>
+    window(Args&& ...args) : p(glfwCreateWindow(std::forward<Args>(args)...)) {}
+
+    bool should_close() {
+      return glfwWindowShouldClose(p.get());
+    }
+  };
+}
+
+void error_callback(int error_code, const char* description) {
+  std::stringstream ss;
+  ss << "GLFW error: " << description;
+  throw std::runtime_error(ss.str());
+}
+
+int main() try {
+  glfwSetErrorCallback(error_callback);
   glfwInit();
-  GLFWwindow *window = glfwCreateWindow(400, 300, "Ingrate", nullptr, nullptr);
-  gladLoadGL(glfwGetProcAddress);
+  {
+    glfw::window window(400, 300, "Ingrate", nullptr, nullptr);
+    while (!window.should_close())
+      glfwWaitEvents();
+  }
   glfwTerminate();
+} catch (std::runtime_error const& e) {
+  std::cerr << e.what() << std::endl;
 }
